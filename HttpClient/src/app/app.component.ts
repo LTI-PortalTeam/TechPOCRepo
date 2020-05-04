e1f931fcb73f60d1a48f82f48a7a2f06bec64b21
NehaSharma-lti <neha1.sharma@lntinfotech.com>
2020-05-04 18:08:15 +0530

    Angular Http Client

---
+++ HttpClient/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MyserviceService } from './myservice.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HttpClient';
+  public persondata = [];
+  constructor(private myservice: MyserviceService) {}
+  ngOnInit() {
+     this.myservice.getData().subscribe((data) => {
+        this.persondata = Array.from(Object.keys(data), k=>data[k]);
+        console.log(this.persondata);
+     });
+  }
+}
