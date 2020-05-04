e3683855d9f456e68e8a44d4198b23f4cc65f16f
WakilahamadLTI <wakil.ahamad@lntinfotech.com>
2020-05-04 16:21:01 +0530

    Spring boot SSO

---
+++ spring-boot-security-sso/sso-server/java/com/sso/ssserver/SsoServerApplication.java
@@ -0,0 +1,17 @@
+package com.sso.ssserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class SsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+
+    
+    
+}
